7e137e5c483937de377dce558475d08920799d29
Alvin <192010003@smatalenta.sch.id>
2024-11-23 20:56:54 +0700

    Perubahan struktur folder

---
+++ data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
+
+INSERT INTO users (username, password, role) VALUES ('admin', 'admin123', 'admin');
+INSERT INTO users (username, password, role) VALUES ('pemilik', 'pemilik123', 'pemilik');
