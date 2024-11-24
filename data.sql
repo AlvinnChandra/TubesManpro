cd7c9cf7a2d5b644ac8d795890be4f8f8aa91df7
Alvin <192010003@smatalenta.sch.id>
2024-11-24 23:04:29 +0700

    Fitur Admin Data Pegawai Sudah Selesai

--- data.sql
+++ data.sql
@@ -14,3 +14,13 @@ CREATE TABLE pegawai (
     nama VARCHAR(100) NOT NULL,
     posisi VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE mesin (
+    id SERIAL PRIMARY KEY,
+    merek VARCHAR(255) UNIQUE NOT NULL,
+    kapasitas INT NOT NULL,
+    tarif INT NOT NULL
+);
+
+INSERT INTO mesin (merek, kapasitas, tarif) VALUES ('LG', '20', '15000');
+INSERT INTO mesin (merek, kapasitas, tarif) VALUES ('Panasonic', '10', '10000');
--- mesin.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE mesin (
-    id SERIAL PRIMARY KEY,
-    merek VARCHAR(255) UNIQUE NOT NULL,
-    kapasitas INT NOT NULL,
-    tarif INT NOT NULL
-);
-
-INSERT INTO mesin (merek, kapasitas, tarif) VALUES ('LG', '20', '15000');
-INSERT INTO mesin (merek, kapasitas, tarif) VALUES ('Panasonic', '10', '10000');
